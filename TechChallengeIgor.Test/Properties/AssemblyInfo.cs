ca6999c3c49bcc5c5291d337c3e6f96ad3c25378
igor de andrade monteiro <igor_fat@hotmail.com>
2018-05-26 10:06:45 -0300

    Include WebApiTest

---
+++ TechChallengeIgor.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TechChallengeIgor.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TechChallengeIgor.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("56ca69f5-1a5f-46da-99ea-8a2ec667ca3c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TechChallengeIgor.Test/WebApiTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TechChallengeIgor.Test
+{
+    [TestClass]
+    public class WebApiTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
